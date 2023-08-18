88c5089c36050848e7eadc9866654514a3e70192
khjgggg <guswjd7@gmail.com>
2023-08-18 23:48:49 +0900

    FIX] : 🔧 회원가입 및 로그인 처리

--- app/src/main/java/com/example/teamproject10/LoginActivity.kt
+++ app/src/main/java/com/example/teamproject10/LoginActivity.kt
@@ -9,7 +9,7 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
-import com.example.teamproject10.data.UserData
+import com.example.teamproject10.data.SignMember
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var resultLauncher: ActivityResultLauncher<Intent>
@@ -22,42 +22,63 @@ class LoginActivity : AppCompatActivity() {
         val btnJoin = findViewById<Button>(R.id.buttonSignUpActivity)
 
         btnLogin.setOnClickListener {
-            val id = editTextId.text.toString()
-            val pw = editTextPw.text.toString()
+//            공백이 포함되었을 경우 대비하여 trim메소드 활용하여 문자열 다루기
 
-            if (id.isNotEmpty() && pw.isNotEmpty()) {
-                val intent = Intent(this,MainActivity::class.java)
+            val id = editTextId.text.toString().trim()
+            val pw = editTextPw.text.toString().trim()
+
+            // isMember함수를 통해서 회원의 로그인 정보가 맞는지 체크
+            if (id.isNotEmpty() && pw.isNotEmpty() && isMember(id, pw)) {
+                val intent = Intent(this, MainActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
-                intent.putExtra("Id",id)
-//                startActivity(intent) //이거하믄 새로 만듬
 
-                //가입한 회원정보와 일치하면 정보를 맴버객체에 담아주고 메인으로 이동시켜야 한다.
-                //지금은 임시로 무조건 이동
-                UserData.id = id
-                UserData.pw = pw
                 setResult(Activity.RESULT_OK, intent)
 
-                Toast.makeText(applicationContext,"로그인 성공",Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(applicationContext, "로그인 성공", Toast.LENGTH_SHORT).show()
                 finish()
-           } else{
-               Toast.makeText(applicationContext,"아이디/비밀번호를 확인해주세요.",Toast.LENGTH_SHORT).show()
-           }
+            } else if (id.isNotEmpty() && pw.isEmpty()) {
+                Toast.makeText(applicationContext, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (id.isEmpty() && pw.isNotEmpty()) {
+                Toast.makeText(applicationContext, "아이디를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            } else if (id.isEmpty() && pw.isEmpty()) {
+                Toast.makeText(applicationContext, "아이디/비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "일치하는 회원정보가 없습니다.", Toast.LENGTH_SHORT).show()
+            }
         }
+
         btnJoin.setOnClickListener {
-            val intent = Intent(this,SignUpActivity::class.java)
+            val intent = Intent(this, SignUpActivity::class.java)
 
             resultLauncher.launch(intent)
         }
 
-        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            if (it.resultCode == RESULT_OK){
-                val id = it.data?.getStringExtra("id")?:""
-                val pw = it.data?.getStringExtra("pw")?:""
-                editTextId.setText(id)
-                editTextPw.setText(pw)
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                if (it.resultCode == RESULT_OK) {
+                    val id = it.data?.getStringExtra("id") ?: ""
+                    val pw = it.data?.getStringExtra("pw") ?: ""
+                    editTextId.setText(id)
+                    editTextPw.setText(pw)
+                }
             }
+    }
+
+    /**
+     * 회원인지 체크하는 함수
+     */
+    private fun isMember(id: String, pw: String): Boolean {
+        //조건에 맞는 회원들을 찾는 코드
+        val result = SignMember.signMemberList.filter {
+            it.id == id && it.pw == pw
         }
 
+        if (result.isNotEmpty()) {
+            SignMember.currentUser = result.first()
+            return true
+        } else {
+            return false
+        }
     }
+
 }
